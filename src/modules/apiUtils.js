da087580c08e268860e86372efd97d8e9127562d
Steve Cirelli <scirelli+git@gmail.com>
2024-12-05 12:52:47 -0500

    quick save

--- src/modules/apiUtils.js
+++ src/modules/apiUtils.js
@@ -19,6 +19,24 @@ function requestUserWatchlists(userId) {
         .then(data=>data.watchlists);
 }
 
+function requestAddUserWatchlist(userId, watchlistData) {
+    return fetch(`/api/user/${userId}/watchlist`, {
+        method:  'POST',
+        cache:   'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect:       'follow',
+        referrerPolicy: 'no-referrer',
+        body:           JSON.stringify(watchlistData)
+    })
+        .then(response=>{
+            if(!response.ok) throw new Error('Request Failed', {cause: response});
+            return response;
+        })
+        .then(response=>response.json());
+}
+
 function requestWatchlist(listId) {
     return fetch(`api/watchlist/${listId}/data.json`, {
         method:   'GET',
@@ -39,4 +57,4 @@ function requestKeyword(keyword) {
     }).then(response=>response.json()).then(result=>{return Array.isArray(result) ? result : [];});
 }
 
-export { requestUserWatchlists, requestWatchlist, requestKeyword};
+export { requestUserWatchlists, requestWatchlist, requestKeyword, requestAddUserWatchlist };
